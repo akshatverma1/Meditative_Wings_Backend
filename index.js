299fcaba7cc136a0d18dc4540d7dc35c6ac1d83d
akshatverma1 <akshaverma14@gmail.com>
2025-03-10 21:51:19 +0530

    All Categories api Done

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require("path");
 const app = express();
 const moongoose = require("mongoose");
 const PORT = 3000;
+const cors = require('cors');
+
+
+app.use(cors());
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -17,7 +22,7 @@ app.get("/", (req, res) => {
 })
 
 async function connectDB() {
-    await moongoose.connect("mongodb://127.0.0.1:27017/test");
+    await moongoose.connect("mongodb+srv://meditativedatabase:pkjYRYQhf0YPyyfs@meditativecluster.h6fcg.mongodb.net/?retryWrites=true&w=majority&appName=Meditativecluster");
 }
 try {
     connectDB().then((result) => {
@@ -28,11 +33,39 @@ try {
 }
 
 const meditative = moongoose.Schema({
-    videoTitle : String,
-    videoDesp :String,
-    thumbnailLink :String,
-    youtubeLink :String,
-    category:String
+    videoTitle: String,
+    videoDesp: String,
+    thumbnailLink: String,
+    youtubeLink: String,
+    category: String
 })
 
-let video = moongoose.model("video",meditative);
+let video = moongoose.model("video", meditative);
+
+let video1 = new video({
+    videoTitle: "Exaple",
+    videoDesp: "Exaple",
+    thumbnailLink: "Exaple",
+    youtubeLink: "Exaple",
+    category: "Exaple"
+})
+
+try {
+    video1.save().then((result) => {
+        console.log("Saved");
+    });
+} catch (err) {
+    console.log(err);
+}
+
+
+app.get("/allcategories", (req, res) => {
+    const chat = async () => {
+        let r = await video.find({});
+        console.log(r);
+        res.json(r);
+    }
+    chat();
+})
+
+
