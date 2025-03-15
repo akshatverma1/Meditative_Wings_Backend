b7815cb47bf5ca81eb8b5fbf7a5b53d121f3594d
akshatverma1 <akshaverma14@gmail.com>
2025-03-16 02:02:25 +0530

    Almost Done

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const cors = require('cors');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
 const bodyParser = require('body-parser');
 app.use(cors());
 
